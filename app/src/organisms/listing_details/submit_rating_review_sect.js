a43681d4fcd311b1035b66f8bd03502860439ebc
jramae gallos <jagallos@up.edu.ph>
2023-05-05 18:11:40 +0800

    update star mui and update buttons

--- app/src/organisms/listing_details/submit_rating_review_sect.js
+++ app/src/organisms/listing_details/submit_rating_review_sect.js
@@ -2,14 +2,14 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './submit_rating_review_sect.css';
 import {InputGroup, Form, Container} from "react-bootstrap";
-import {StarRating} from '../../atoms'
+import {MUIStarRating} from '../../atoms'
 
 const SubmitRatingReviewSect = () => {
     return(
         <Container className="comment-star-container">
             <InputGroup className="comment-button">
                 <div className="star-div">
-                    <StarRating/>
+                    <MUIStarRating/>
                 </div>
                 <Form.Control
                     placeholder="Write Comment"
