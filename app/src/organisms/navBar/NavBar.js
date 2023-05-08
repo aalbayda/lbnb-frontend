dedd0e0f57068e58a9fc86bacc21993999ffecee
bob <aalbayda@up.edu.ph>
2023-05-08 23:32:37 +0800

    fixed proxy server bug

--- app/src/organisms/navBar/NavBar.js
+++ app/src/organisms/navBar/NavBar.js
@@ -10,37 +10,39 @@ const NavBar = () => {
   const [signedIn, setSignedIn] = useState(false);
   const [modalShow, setModalShow] = useState(false);
 
-  const checkCookie = () => {
-    let authToken = "";
-    const tokens = document.cookie.split(" ");
-    for (let token in tokens) {
-      let newToken = token.split("=");
-      if (newToken[0] === "authToken") authToken = newToken[1];
-    }
+  // const checkCookie = () => {
+  //   const authToken = document.cookie.split(" ")[1].split("=")[1];
+  //   // console.log(tokens)
+  //   // for (let token in tokens) {
+  //   //   console.log(token)
+  //   //   let newToken = token.split("=");
+  //   //   console.log(newToken);
+  //   //   if (newToken[0] === "authToken") authToken = newToken[1];
+  //   // }
 
-    console.log(authToken)
+  //   console.log(authToken)
 
-    axios.post(url+'/login', {
-      authToken: authToken
-    })
-    .then(function (response) {
-      console.log(response.data);
-      // if (!response.data) {
-      //   setInvalidLogin(true);
-      // }
-      // else {
-      //   console.log(response.data)
-      //   document.cookie = `authToken=${response.data.authToken}; path=/;`;
-      //   window.location.reload(); 
-      // }
-      return true;
-    })
-    .catch(function (error) {
-      console.log("Error!!!");
-      console.log(error);
-      return false;
-    });
-  }
+  //   axios.post(url+'/login', {
+  //     authToken: authToken
+  //   })
+  //   .then(function (response) {
+  //     console.log(response.data);
+  //     // if (!response.data) {
+  //     //   setInvalidLogin(true);
+  //     // }
+  //     // else {
+  //     //   console.log(response.data)
+  //     //   document.cookie = `authToken=${response.data.authToken}; path=/;`;
+  //     //   window.location.reload(); 
+  //     // }
+  //     return true;
+  //   })
+  //   .catch(function (error) {
+  //     console.log("Error!!!");
+  //     console.log(error);
+  //     return false;
+  //   });
+  // }
 
   return (
     <div className="navbar-container">
@@ -60,14 +62,14 @@ const NavBar = () => {
         </div>
         <div className="navbar-right">
           <p className="small">About</p>
-
-            {
-              signedIn ?
-               <Button className="login-btn">Logout</Button> 
-               : 
                <Button className="login-btn" onClick={() => setModalShow(true)}>
                 Login
-              </Button> }
+              </Button> 
+
+            {/* {
+              checkCookie() ?
+               <Button className="login-btn">Logout</Button> 
+               :  */}
 
               {/* <Button onClick={checkCookie}>test</Button> */}
             
--- app/src/setupProxy.js
+++ app/src/setupProxy.js
@@ -1,11 +1,5 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-// module.exports = function(app) {
-//     app.use(
-//       '/api',
-//       createProxyMiddleware({
-//         target: 'https://mockup-backend-128.herokuapp.com',
-//         changeOrigin: true,
-//       })
-//     );
-//   };
-  
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(createProxyMiddleware('/proxypath', { target: '<target path>' }));
+};
