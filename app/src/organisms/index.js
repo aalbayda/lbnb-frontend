4e3251313b582bd8948a574a345214c80874d792
bob <aalbayda@up.edu.ph>
2023-06-05 11:30:28 +0800

    fix: temporarily removed carousels for deploy

--- app/src/organisms/index.js
+++ app/src/organisms/index.js
@@ -5,8 +5,6 @@ export { default as CardListingAddRoom } from "./cardListing_addRoom/CardListing
 export { default as Banner } from "./banner/Banner";
 export { default as Footer } from "./footer/Footer";
 export { default as ApartmentCarousel } from "./apartment_carousel/Apartment_carousel";
-export { default as HotelsCarousel } from "./hotels_carousel/Hotels_carousel";
-export { default as DormCarousel } from "./dorm_carousel/Dorm_carousel";
 export { default as Multilayerfilter } from "./multilayer_filter/Multilayer_filter";
 export { default as DevTile } from "./devTile/DevTile";
 
