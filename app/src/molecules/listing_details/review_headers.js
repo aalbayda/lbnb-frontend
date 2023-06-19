23b4d92d4170459010330e7cc3b53c1fcc2f6d46
Gnet <gbbalucio@up.edu.ph>
2023-06-19 19:01:07 +0800

    style: changed rating button style and fixed half-star alignment

--- app/src/molecules/listing_details/review_headers.js
+++ app/src/molecules/listing_details/review_headers.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./review_headers.css";
 import config from "../../config";
 import { Container } from "react-bootstrap";
+import { Rating } from "@mui/material";
 const url = config.apiUrl;
 
 const ReviewHeaders = () => {
@@ -10,14 +11,87 @@ const ReviewHeaders = () => {
 
   return (
     <Container className="review-container">
-      <h4 className="review-header">{header}</h4>
+      <Container className="review-header-container">
+        <h4 className="review-header">{header}</h4>
+      </Container>
+
       <Container className="review-numstar-button">
-        <button className="all s-add-style"> All </button>
-        <button className="s1 s-add-style"> 5 Star</button>
-        <button className="s2 s-add-style"> 4 Star</button>
-        <button className="s3 s-add-style"> 3 Star</button>
-        <button className="s4 s-add-style"> 2 Star</button>
-        <button className="s5 s-add-style"> 1 Star</button>
+        <button className="all s-add-style"> 
+          <Rating
+              className="rating-medium"
+              defaultValue={5}
+              readOnly={true}
+              sx={{
+                fontSize: "1rem",
+                color: "var(--color-background)",
+              }}
+            />
+          <p>All</p>
+        </button>
+        <button className="s1 s-add-style"> 
+          <Rating
+            className="rating-medium"
+            defaultValue={5}
+            readOnly={true}
+            sx={{
+              fontSize: "1rem",
+              color: "var(--color-background)",
+            }}
+          />
+          <p>5 Star</p>
+        </button>
+        <button className="s2 s-add-style"> 
+          <Rating
+            className="rating-medium"
+            defaultValue={4}
+            max={4}
+            readOnly={true}
+            sx={{
+              fontSize: "1rem",
+              color: "var(--color-background)",
+            }}
+          />  
+          <p>4 Star</p>
+        </button>
+        <button className="s3 s-add-style"> 
+          <Rating
+            className="rating-medium"
+            defaultValue={3}
+            max={3}
+            readOnly={true}
+            sx={{
+              fontSize: "1rem",
+              color: "var(--color-background)",
+            }}
+          />    
+          <p>3 Star</p>
+        </button>
+        <button className="s4 s-add-style"> 
+          <Rating
+            className="rating-medium"
+            defaultValue={2}
+            max={2}
+            readOnly={true}
+            sx={{
+              fontSize: "1rem",
+              color: "var(--color-background)",
+            }}
+          />  
+          <p>2 Star</p>
+        </button>
+        <button className="s5 s-add-style">
+          <Rating
+            className="rating-medium"
+            defaultValue={1}
+            max={1}
+            readOnly={true}
+            sx={{
+              fontSize: "1rem",
+              color: "var(--color-background)",
+            }}
+          />  
+          <p>1 Star</p>
+        </button>
       </Container>
     </Container>
   );
