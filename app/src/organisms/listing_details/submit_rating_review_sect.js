cd8e87b86207ff2e51ad0e9bff601ea01112ad3d
bob <aalbayda@up.edu.ph>
2023-06-20 12:57:49 +0800

    feat: user notified of review submission success

--- app/src/organisms/listing_details/submit_rating_review_sect.js
+++ app/src/organisms/listing_details/submit_rating_review_sect.js
@@ -63,6 +63,16 @@ const SubmitRatingReviewSect = (props) => {
 				})
 				.then(function (response) {
 					console.log(response);
+					if (response.success) {
+						setAlert("Review successfully submitted!");
+						setModalShow(true);
+						setTimeout(() => window.location.reload(), 30000);
+					} else {
+						setAlert(
+							"Review unsuccessful. Check your internet or server connection."
+						);
+						setModalShow(true);
+					}
 				})
 				.catch(function (error) {
 					console.log("Error");
@@ -73,62 +83,61 @@ const SubmitRatingReviewSect = (props) => {
 		setRateVal(0);
 	};
 
-  return (
-    <Container className="comment-star-container">
-      <h4 className="write-review-header">Write a Review</h4>
-      <Row>
-        <div className="star-div">
-        <Table
-          sx={{
-            [`& .${tableCellClasses.root}`]: {
-              borderBottom: "none"
-            },
-            width: '10%'
-          }}
-        >
-          <TableRow>
-            <TableCell align="left">
-              <Rating
-                className="rating-medium"
-                // defaultValue={3.5}
-                precision={0.5}
-                // disabled={isLoggedIn() && getAuthType === "Student" ? false : true}
-                sx={{
-                  fontSize: "2rem",
-                  color: "#1C3103",
-                }}
-                onChange={(newValue) =>
-                  setRateVal(parseFloat(newValue.target.value))
-                }
-                value={rateVal}
-                
-              />
-            </TableCell>
-          </TableRow>
-        </Table>
-          <textarea
-            className="comment-textfield"
-            // sx={{
-            //   width: 950,
-            // }}
-            id="fullWidth"
-            placeholder="Got something else to say? Type here!"
-            multiline
-            // disabled={!(isLoggedIn() && getAuthType === "Student")}
-            maxRows={4}
-            onChange={(newValue) => setComment(newValue.target.value)}
-            value={comment}
-          />
-          <button className="submit-button" onClick={handleSubmit}>
-            {" "}
-            Submit{" "}
-          </button>
-          <AlertModals
-            alert = {alert}
-            show={modalShow} 
-            onHide={() => setModalShow(false)} 
-          />
-          {/* {isLoggedIn() && getAuthType() === "Student" ? (
+	return (
+		<Container className="comment-star-container">
+			<h4 className="write-review-header">Write a Review</h4>
+			<Row>
+				<div className="star-div">
+					<Table
+						sx={{
+							[`& .${tableCellClasses.root}`]: {
+								borderBottom: "none",
+							},
+							width: "10%",
+						}}
+					>
+						<TableRow>
+							<TableCell align="left">
+								<Rating
+									className="rating-medium"
+									// defaultValue={3.5}
+									precision={0.5}
+									// disabled={isLoggedIn() && getAuthType === "Student" ? false : true}
+									sx={{
+										fontSize: "2rem",
+										color: "#1C3103",
+									}}
+									onChange={(newValue) =>
+										setRateVal(parseFloat(newValue.target.value))
+									}
+									value={rateVal}
+								/>
+							</TableCell>
+						</TableRow>
+					</Table>
+					<textarea
+						className="comment-textfield"
+						// sx={{
+						//   width: 950,
+						// }}
+						id="fullWidth"
+						placeholder="Got something else to say? Type here!"
+						multiline
+						// disabled={!(isLoggedIn() && getAuthType === "Student")}
+						maxRows={4}
+						onChange={(newValue) => setComment(newValue.target.value)}
+						value={comment}
+					/>
+					<button className="submit-button" onClick={handleSubmit}>
+						{" "}
+						Submit{" "}
+					</button>
+					<AlertModals
+						alert={alert}
+						show={modalShow}
+						onHide={() => setModalShow(false)}
+					/>
+					{/* {isLoggedIn() && getAuthType() === "Student" ? (
 	return (
 		<Container className="comment-star-container">
 			<h4 className="write-review-header">Write a Review</h4>
