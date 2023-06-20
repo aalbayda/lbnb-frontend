008f9f6060bf3e837c1fb905bc5b6ab21eff5b51
Remedey <rmdayahan@up.edu.ph>
2023-06-21 02:42:32 +0800

    Test: Search Component

--- app/cypress/e2e/main_tests/Search_filter_test.cy.js
+++ app/cypress/e2e/main_tests/Search_filter_test.cy.js
@@ -1,9 +1,42 @@
 import { convertLength } from "@mui/material/styles/cssUtils";
 
-describe('Guest Accommodation Search Filter Test', () => {
-    it('Should filter the available accommodations using Accommodation Type, Price, Capacity, and Location through a Guest Account successfully', () => {
+describe('Accommodation Search Bar Test', () => {
+    it('Verify that the search bar is displayed on the home page.', () => {
       // Visit Website
       cy.visit('http://localhost:3000');
+
+
+    // Click Search Button
+    cy.get('input[testID=searchField]')
+        .should('be.visible')
+
+        cy.wait(2000);
+    })
+  })
+
+
+describe('Accommodation Search No Preference Test', () => {
+    it('Verify that the user can view all listings when no filters are applied.', () => {
+      // Visit Website
+      cy.visit('http://localhost:3000');
+
+      cy.wait(2000);
+
+    // Click Search Button
+    cy.get('button[testID=searchButton]')
+        .should('be.visible')
+        .click();
+        cy.wait(2000);
+    })
+  })
+
+
+describe('Accommodation Search Filter Test', () => {
+    it('Should filter the available accommodations using Accommodation Type, Price, Capacity, and Location successfully', () => {
+      // Visit Website
+      cy.visit('http://localhost:3000');
+    
+      cy.wait(10000);
 
     // Enter Text into Search Field
     cy.get('input[testID=searchField]')
@@ -77,5 +110,32 @@ describe('Guest Accommodation Search Filter Test', () => {
         .get('button[testID=searchButton]')
         .should('be.visible')
         .click();
+
+        cy.wait(2000);
     })
   })
+
+
+describe('Name Search Filter Test', () => {
+    it('Should filter the available accommodations name successfully', () => {
+      // Visit Website
+      cy.visit('http://localhost:3000');
+
+      cy.wait(10000);
+
+    // Enter Text into Search Field
+    cy.get('input[testID=searchField]')
+        .should('be.visible')
+        .type("Kael");
+
+    // Click Search Button
+    cy.get('button[testID=searchButton]')
+        .should('be.visible')
+        .click();
+
+    cy.wait(2000);
+
+
+    })
+})  
+
