1e15b99363e3be57339f91a955971959050aadbe
Remedey <rmdayahan@up.edu.ph>
2023-06-22 01:03:25 +0800

    fix: misspelling

--- app/cypress/e2e/main_tests/generate_summary_test.cy.js
+++ app/cypress/e2e/main_tests/generate_summary_test.cy.js
@@ -1,6 +1,6 @@
 import { convertLength } from "@mui/material/styles/cssUtils";
 
-describe('Generate Report Test', () => {
+describe('Generate Summary Test', () => {
     it('Verify that the user should be able to view and/or download the list of filtered accommodations based on their query.', () => {
       // Visit Website
       cy.visit('http://localhost:3000');
@@ -12,11 +12,14 @@ describe('Generate Report Test', () => {
         .should('be.visible')
         .click();
     
+    cy.get('button[testID=searchButton]')
+    .should('be.visible')
+    .click();     
     cy.wait(2000);
 
-    cy.get('button[testID=generateSummary]')
-    .should('be.visible')
-    .click();
+    // cy.get('button[testID=generateSummary]')
+    // .should('be.visible')
+    // .click();
     
     cy.wait(2000);
 
